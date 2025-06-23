f8f94c1dba47af9669cdd3e7c0965bfe5c0a1b09
Mantu Kumar <mantu911711@gmail.com>
2025-06-23 16:22:22 +0530

    fix: Correct error handling in note retrieval API

--- src/app/api/note/[noteId]/route.ts
+++ src/app/api/note/[noteId]/route.ts
@@ -2,13 +2,15 @@ import prisma from "@/config/dbConfig";
 import { getDataFromToken } from "@/helper/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest,{ params }: { params: { noteId: string }}) {
-  try {
+export async function GET(req:NextRequest
     const userId=await getDataFromToken(req);
     if(!userId){
       return NextResponse.json({message:'Unautherize'},{status:401});
     }
-    const {noteId}=await params;
+    
+    const url = new URL(req.url);
+    const noteId = url.pathname.split('/').pop();
+
     const chat=await prisma.note.findFirst({
       where:{id:noteId},
       include:{contenets:true}
